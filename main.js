a7e215e64140a9fc1c58c51210b212a07284d55d
PicEncrypt Project <98749433+picencrypt@users.noreply.github.com>
2023-02-04 04:08:40 +0800

    Redesign to MS UI

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
+    width: 400,
     height: 600,
     icon:'./build/icons/default.png',
     title:"PicEncrypt",
+    frame: false,
+resizable: false,
   })
   
-  win.loadFile('index.html')
+  win.loadFile('main.html')
 }
 
 app.whenReady().then(() => {
